eb080b5f418eaeacbd2e3e2e83e04802b2a09379
RICKY BROWN <DTRB@RICKYs-MacBook-Pro.local>
2014-01-18 21:43:59 -0500

    .

--- LeaveATrace/AppDelegate.m
+++ LeaveATrace/AppDelegate.m
@@ -17,6 +17,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     
+     
     
     _tracesArray = [[NSMutableArray alloc] init];
     
